aad8fe97cde17a40b8d7c5b9e52b245d29580cbd
evertras <bfullj@gmail.com>
2015-03-30 19:18:18 -0400

    Much better hero listing

--- src/controllers/heroListCtrl.js
+++ src/controllers/heroListCtrl.js
@@ -3,5 +3,27 @@
 
 	app.controller('heroListCtrl', ['heroDataService', '$scope', function(heroDataService, $scope) {
 		$scope.allHeroes = heroDataService.allHeroes;
+
+		$scope.allHeroChunks = [];
+
+		$scope.$watch(function(scope) { return scope.allHeroes.length; },
+			function() {
+				console.log("Updating chunks...");
+				$scope.allHeroChunks = [];
+
+				var chunks = 3;
+
+				var split = Math.floor($scope.allHeroes.length / 3);
+
+				console.log("Split is " + split);
+
+				if (split > 1) {
+					var sortedHeroes = _.sortBy($scope.allHeroes, 'name');
+
+					for (var i = 0; i < chunks; ++i) {
+						$scope.allHeroChunks.push(sortedHeroes.slice(split*i, split*i + split));
+					}
+				}
+			});
 	}]);
 })();
