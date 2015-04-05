9c04acdeceab70960150715d89954aede5f6d2e7
evertras <bfullj@gmail.com>
2015-04-04 21:28:24 -0400

    Added share buttons with GA tracking

--- src/controllers/heroDetailCtrl.js
+++ src/controllers/heroDetailCtrl.js
@@ -1,11 +1,21 @@
 (function() {
 	var app = angular.module('heroDetailCtrl', ['ngRoute', 'heroDataService', 'tipList', 'utilDataService']);
 
-	app.controller('heroDetailCtrl', ['$scope', 'heroDataService', '$routeParams', 'utilDataService', 
-		function($scope, heroDataService, $routeParams, utilDataService) {
+	app.controller('heroDetailCtrl', ['$scope', 'heroDataService', '$routeParams', 'utilDataService', '$rootScope', '$window',
+		function($scope, heroDataService, $routeParams, utilDataService, $rootScope, $window) {
 			$scope.hero = heroDataService.getHeroByID($routeParams.heroId);
 			$scope.counters = heroDataService.getCountersForHero($routeParams.heroId);
 			$scope.mySource = utilDataService.mySource;
+
+			$rootScope.$watch(function() { return $scope.hero.name; }, function() {
+				if ($scope.hero.name) {
+					$rootScope.title = ' - ' + $scope.hero.name;
+				} else {
+					$rootScope.title = '';
+				}
+			});
+
+			$rootScope.title = ' - ' + $scope.hero.name;
 
 			$scope.upvote = function(counter) {
 				heroDataService.upvoteCounter(counter);
@@ -35,6 +45,12 @@
 
 				$scope.helperText = "";
 			};
+
+			$("#share-buttons a").click(function (button) {
+				var sharedTo = $(this).children('img').attr('alt');
+
+				$window.ga('send', 'event', 'Shared', sharedTo, $scope.hero.name);
+			});
 		}
 	]);
 })();
