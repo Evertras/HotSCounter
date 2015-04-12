c64cef938ef0f40264919cfac84f34ca7efae454
evertras <bfullj@gmail.com>
2015-04-12 17:42:06 -0400

    Using net total filter for template view

--- src/filters/voteFilters.js
+++ src/filters/voteFilters.js
@@ -1,6 +1,17 @@
 (function() {
 	var app = angular.module('voteFilters', []);
 
+	app.filter('totalNetVotes', function () {
+		return function (tip) {
+			return tip.votes.map(function(vote) {
+					return vote.isUpvote ? 1 : -1;
+				}).
+				reduce(function(prev, cur) {
+					return prev + cur;
+				}, 0);
+		};
+	});
+
 	app.filter('totalUpvotes', function () {
 		return function (tip) {
 			return tip.votes.map(function(vote) {
@@ -38,7 +49,7 @@
 
 				return Math.floor(percentUpvotes * 100) + '%';
 			} else {
-				return 0;
+				return '0%';
 			}
 		};
 	});
