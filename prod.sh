5e0915a8028871170048e911ce64eef1992666f3
Brandon Fulljames <bfullj@gmail.com>
2017-05-29 18:29:26 -0700

    Better prod script

--- prod.sh
+++ prod.sh
@@ -3,6 +3,7 @@ if [ $# -ne 1 ]; then
   echo 'Usage: prod.sh [version]'
   exit 1
 fi
+docker pull evertras/hotscounter:${1} || exit 1
 docker stop hotscounter || echo 'fine'
 docker rm hotscounter || echo 'fine'
 docker run -d --name hotscounter --restart always --link mongo:mongo -p 80:8080 evertras/hotscounter:${1}
