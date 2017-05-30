ba803dadbc2f8980c1c703a1fb73945a596061f4
Brandon Fulljames <bfullj@gmail.com>
2017-05-29 18:21:00 -0700

    Prefix docker image name to pull from dockerhub

--- prod.sh
+++ prod.sh
@@ -5,4 +5,4 @@ if [ $# -ne 1 ]; then
 fi
 docker stop hotscounter || echo 'fine'
 docker rm hotscounter || echo 'fine'
-docker run -d --name hotscounter --restart always --link mongo:mongo -p 80:8080 hotscounter:${1}
+docker run -d --name hotscounter --restart always --link mongo:mongo -p 80:8080 evertras/hotscounter:${1}
